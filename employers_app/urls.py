6fee63122f75001148127db4ffb43345956da085
timama <timforworking@mail.ru>
2024-01-30 12:49:22 +0300

    push

--- employers_app/urls.py
+++ employers_app/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from employers_app.views import EmployerViewSet
 
 router = DefaultRouter()
-router.register(r'employers', EmployerViewSet)
+router.register(r'', EmployerViewSet)
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
