0a0754ea724a098701ce284ffc30c1f6be2d76e9
timama <timforworking@mail.ru>
2024-01-31 12:34:28 +0300

    push

--- employers_app/admin.py
+++ employers_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Employer
 
-# Register your models here.
+
+@admin.register(Employer)
+class EmployerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'inn', 'legal_address', 'contact_person_fio', 'phone_number', 'contact_person_email')
+    search_fields = ('name', 'inn', 'contact_person_fio', 'phone_number', 'contact_person_email')
+    list_filter = ('name',)
