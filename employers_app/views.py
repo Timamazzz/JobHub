ec8c300e5e96a639d8404e4920df5e0152a95224
timama <timforworking@mail.ru>
2024-01-31 16:20:59 +0300

    push

--- employers_app/views.py
+++ employers_app/views.py
@@ -31,7 +31,7 @@ class EmployerViewSet(ModelViewSet):
             html_message = f'<p>Data for moderation has been received:</p><pre>{serializer.data}</pre>'
 
             mail.send(
-                'artem_galkin_01@list.ru',
+                '89205731783@mail.ru',
                 settings.DEFAULT_FROM_EMAIL,
                 subject=subject,
                 message=message,
