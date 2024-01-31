c91f6ad331421f2011469f53c7180d5c7205a342
timama <timforworking@mail.ru>
2024-01-31 16:25:15 +0300

    push

--- users_app/admin.py
+++ users_app/admin.py
@@ -1,23 +1,56 @@
-from django.contrib import admin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import UserChangeForm
-
 from users_app.models import User
+from django.contrib import admin
 
 
-class UserChangeForm(UserChangeForm):
+class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = User
 
 
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+
 class CustomUserAdmin(BaseUserAdmin):
-    form = UserChangeForm
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('username', 'password1', 'password2'),
         }),
     )
+
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Use special forms for user creation and change.
+        """
+        if obj is None:
+            return self.add_form
+        return super().get_form(request, obj, **kwargs)
+
+    def save_model(self, request, obj, form, change):
+        """
+        Use the correct manager for user creation and update.
+        """
+        if not change:
+            obj.set_password(form.cleaned_data['password1'])
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(User, CustomUserAdmin)
