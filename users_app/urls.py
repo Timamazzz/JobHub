07fc44422e6b2f8c6f745b0fe0f56e857ba6750b
timama <timforworking@mail.ru>
2024-02-02 16:46:24 +0300

    push

--- users_app/urls.py
+++ users_app/urls.py
@@ -10,6 +10,6 @@ router.register(r'', UserViewSet)
 urlpatterns = [
     path('social/', include('social_django.urls')),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('my/login/vk/', VKLoginView.as_view(), name='vk-login'),
+    path('login/vk/', VKLoginView.as_view(), name='vk-login'),
     path('', include(router.urls)),
 ]
