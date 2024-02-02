0a8166f962630f5924971095ba8931384d3f52d7
timama <timforworking@mail.ru>
2024-02-02 16:45:22 +0300

    push

--- users_app/urls.py
+++ users_app/urls.py
@@ -8,8 +8,8 @@ router = DefaultRouter()
 router.register(r'', UserViewSet)
 
 urlpatterns = [
-    path('login/vk/', VKLoginView.as_view(), name='vk-login'),
     path('social/', include('social_django.urls')),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/vk/', VKLoginView.as_view(), name='vk-login'),
     path('', include(router.urls)),
 ]
