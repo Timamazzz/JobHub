e077427669c7b90db52b4902b4e6e894ad765b6e
timama <timforworking@mail.ru>
2024-02-02 16:27:43 +0300

    push

--- users_app/urls.py
+++ users_app/urls.py
@@ -2,12 +2,13 @@ import social_django
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
-from users_app.views import UserViewSet
+from users_app.views import UserViewSet, VKLoginView
 
 router = DefaultRouter()
 router.register(r'', UserViewSet)
 
 urlpatterns = [
+    path('api/auth/social/vk/', VKLoginView.as_view(), name='vk-login'),
     path('social/', include('social_django.urls')),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
--- users_app/views.py
+++ users_app/views.py
@@ -1,9 +1,23 @@
+from django.http import JsonResponse
+from django.views import View
+from social_django.utils import psa
+
 from JobHub.utils.ModelViewSet import ModelViewSet
 from users_app.models import User
 from users_app.serializers.user_serializers import UserSerializer, UserRetrieveSerializer
 
 
 # Create your views here.
+class VKLoginView(View):
+    @psa('social:begin')
+    def post(self, request, *args, **kwargs):
+        return self.render_json_response({'redirect_url': request.backend.auth_url()})
+
+    def render_json_response(self, data, status=200):
+        print('data', data)
+        return JsonResponse(data, status=status)
+
+
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
