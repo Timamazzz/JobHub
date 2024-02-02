6e6b18523e34cb55bf5ad31f38cf524af0425c7b
timama <timforworking@mail.ru>
2024-02-02 15:48:17 +0300

    push

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
-
+from social_django.urls import urlpatterns as social_django_urls
 from users_app.views import UserViewSet
 
 router = DefaultRouter()
 router.register(r'', UserViewSet)
 
 urlpatterns = [
+    path('auth/social/', include(social_django_urls, namespace='social')),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
