106fa70648758421c484ed1883c05094f2740273
timama <timforworking@mail.ru>
2024-02-05 18:05:28 +0300

    push

--- docs_app/admin.py
+++ docs_app/admin.py
@@ -1,6 +1,7 @@
-# from django.contrib import admin
-#
-# from docs_app.models import EventImage, ExcursionImage
+from django.contrib import admin
+from docs_app.models import *
+
+
 #
 #
 # @admin.register(EventImage)
@@ -13,3 +14,8 @@
 # class PhotoExcursionAdmin(admin.ModelAdmin):
 #     list_display = ('original_name', 'excursion', 'upload_time', 'is_preview')
 #     search_fields = ('original_name', 'excursion__text')
+
+@admin.register(ApplicantAvatar)
+class ApplicantAvatarAdmin(admin.ModelAdmin):
+    list_display = ('original_name', 'excursion', 'upload_time', 'is_preview')
+    search_fields = ('original_name', 'excursion__text')
