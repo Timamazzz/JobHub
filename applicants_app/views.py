fd08033dd24c9c9b7b33db3e2b49c0c0a15941a6
timama <timforworking@mail.ru>
2024-02-05 17:10:38 +0300

    push

--- applicants_app/views.py
+++ applicants_app/views.py
@@ -1,5 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from JobHub.utils.ModelViewSet import ModelViewSet
@@ -20,4 +23,12 @@ class ApplicantViewSet(ModelViewSet):
     serializer_list = {
         'retrieve': ApplicantRetrieveSerializer,
         'update': ApplicantUpdateSerializer,
+        'get-by-user': ApplicantRetrieveSerializer,
     }
+
+    @action(detail=False, methods=['GET'], url_path='get-by-user')
+    def get_by_user(self, request):
+        user = request.user
+        queryset = Applicant.objects.filter(user=user)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
