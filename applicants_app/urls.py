673fdaa4785d033b337aba464a040809fed1fe32
timama <timforworking@mail.ru>
2024-02-05 14:31:55 +0300

    push

--- applicants_app/urls.py
+++ applicants_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from applicants_app.views import ApplicantViewSet
+
 router = DefaultRouter()
+router.register(r'', ApplicantViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
