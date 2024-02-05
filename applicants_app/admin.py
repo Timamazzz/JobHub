2b60b3e7e96e39ce165f56ef479c36d30ecc18ef
timama <timforworking@mail.ru>
2024-02-05 14:04:19 +0300

    push

--- applicants_app/admin.py
+++ applicants_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Applicant
 
-# Register your models here.
+
+class ApplicantAdmin(admin.ModelAdmin):
+    list_display = ('user', 'fio', 'birth_date', 'phone_number', 'email', 'vk_id')
+    search_fields = ('user__username', 'fio', 'phone_number', 'email', 'vk_id')
+    list_filter = ('birth_date',)
+
+
+admin.site.register(Applicant, ApplicantAdmin)
