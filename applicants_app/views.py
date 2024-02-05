223fbc9d54723f9a9fedc450f316553560b34785
timama <timforworking@mail.ru>
2024-02-05 14:31:12 +0300

    push

--- applicants_app/views.py
+++ applicants_app/views.py
@@ -1,3 +1,6 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from JobHub.utils.ModelViewSet import ModelViewSet
 from applicants_app.models import Applicant
 from applicants_app.serializers.applicant_serializers import ApplicantSerializer, ApplicantRetrieveSerializer, \
@@ -8,6 +11,8 @@ from applicants_app.serializers.applicant_serializers import ApplicantSerializer
 class ApplicantViewSet(ModelViewSet):
     queryset = Applicant.objects.all()
     serializer_class = ApplicantSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_list = {
         'retrieve': ApplicantRetrieveSerializer,
         'update': ApplicantUpdateSerializer,
