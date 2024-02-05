b97cbb86666c1adb2dcccc6e5d861e5c6d08b160
timama <timforworking@mail.ru>
2024-02-05 12:25:01 +0300

    push

--- users_app/views.py
+++ users_app/views.py
@@ -81,6 +81,7 @@ class UserViewSet(ModelViewSet):
         response = requests.get('https://oauth.vk.com/access_token', params={
             'client_id': SOCIAL_AUTH_VK_OAUTH2_KEY,
             'client_secret': SOCIAL_AUTH_VK_OAUTH2_SECRET,
+            'redirect_uri': '',
             'code': code
         })
         data = response.json()
