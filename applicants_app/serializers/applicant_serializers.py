93fa402d6d42c7bb381d893dfef2aeb61e443625
timama <timforworking@mail.ru>
2024-02-06 17:09:09 +0300

    push

--- applicants_app/serializers/applicant_serializers.py
+++ applicants_app/serializers/applicant_serializers.py
@@ -32,7 +32,79 @@ class ApplicantCreateSerializer(serializers.ModelSerializer):
 
 
 class ApplicantUpdateSerializer(serializers.ModelSerializer):
+    avatar = ApplicantCreateAvatarSerializer()
     phone_number = PhoneField()
+
+    def create(self, validated_data):
+        raise_errors_on_nested_writes('create', self, validated_data)
+
+        ModelClass = self.Meta.model
+
+        info = model_meta.get_field_info(ModelClass)
+        many_to_many = {}
+        for field_name, relation_info in info.relations.items():
+            if relation_info.to_many and (field_name in validated_data):
+                many_to_many[field_name] = validated_data.pop(field_name)
+
+        try:
+            instance = ModelClass._default_manager.create(**validated_data)
+        except TypeError:
+            tb = traceback.format_exc()
+            msg = (
+                    'Got a `TypeError` when calling `%s.%s.create()`. '
+                    'This may be because you have a writable field on the '
+                    'serializer class that is not a valid argument to '
+                    '`%s.%s.create()`. You may need to make the field '
+                    'read-only, or override the %s.create() method to handle '
+                    'this correctly.\nOriginal exception was:\n %s' %
+                    (
+                        ModelClass.__name__,
+                        ModelClass._default_manager.name,
+                        ModelClass.__name__,
+                        ModelClass._default_manager.name,
+                        self.__class__.__name__,
+                        tb
+                    )
+            )
+            raise TypeError(msg)
+        if many_to_many:
+            for field_name, value in many_to_many.items():
+                field = getattr(instance, field_name)
+                field.set(value)
+
+        avatar_data = self.initial_data.get('avatar')
+        if avatar_data:
+            avatar_serializer = ApplicantCreateAvatarSerializer(data=avatar_data)
+            if avatar_serializer.is_valid():
+                avatar_serializer.save(applicant=instance)
+
+        return instance
+
+    def update(self, instance, validated_data):
+        raise_errors_on_nested_writes('update', self, validated_data)
+        info = model_meta.get_field_info(instance)
+        m2m_fields = []
+        for attr, value in validated_data.items():
+            if attr in info.relations and info.relations[attr].to_many:
+                m2m_fields.append((attr, value))
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        for attr, value in m2m_fields:
+            field = getattr(instance, attr)
+            field.set(value)
+
+        avatar_data = validated_data.get('avatar')
+        if avatar_data:
+            avatar_instance = instance.avatar
+            if avatar_instance:
+                avatar_serializer = ApplicantCreateAvatarSerializer(instance=avatar_instance, data=avatar_data)
+            else:
+                avatar_serializer = ApplicantCreateAvatarSerializer(data=avatar_data)
+            if avatar_serializer.is_valid():
+                avatar_serializer.save(applicant=instance)
+
+        return instance
 
     class Meta:
         model = Applicant
