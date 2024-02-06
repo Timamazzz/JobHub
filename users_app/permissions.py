115e1894c2b37f61f6cf17c774240318b151f3fb
timama <timforworking@mail.ru>
2024-02-06 11:46:14 +0300

    push

--- users_app/permissions.py
+++ users_app/permissions.py
@@ -4,9 +4,16 @@ from users_app.enums import UserRoleEnum
 
 class IsApplicant(permissions.BasePermission):
     def has_permission(self, request, view):
+        if request.method == 'OPTIONS':
+            return True
+
         return request.user and request.user.is_authenticated and request.user.role == UserRoleEnum.APPLICANT.value
 
 
 class IsEmployer(permissions.BasePermission):
     def has_permission(self, request, view):
+        if request.method == 'OPTIONS':
+            return True
+
         return request.user and request.user.is_authenticated and request.user.role == UserRoleEnum.EMPLOYER.value
+
