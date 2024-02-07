3940b3c28d6cd13bd011524e14674f2981089eb7
timama <timforworking@mail.ru>
2024-02-07 12:29:01 +0300

    push

--- docs_app/serializers/ApplicantAvatarSerializer.py
+++ docs_app/serializers/ApplicantAvatarSerializer.py
@@ -4,25 +4,14 @@ from docs_app.models import ApplicantAvatar
 
 
 class ApplicantRetrieveAvatarSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = ApplicantAvatar
         fields = ('id', 'file', 'original_name', 'upload_time', 'extension')
 
 
 class ApplicantCreateOrUpdateAvatarSerializer(serializers.ModelSerializer):
-    #file = serializers.CharField(max_length=1024)
-    file = serializers.SerializerMethodField()
-
-    def get_file(self, obj):
-        if obj.file:
-            file_url = obj.file.url
-            # Получаем путь после 'media/'
-            file_path = file_url.split('media/', 1)[-1]
-            return file_path
-        return None
+    file = serializers.CharField(max_length=1024)
 
     class Meta:
         model = ApplicantAvatar
         fields = ('id', 'file', 'original_name', 'extension')
-
