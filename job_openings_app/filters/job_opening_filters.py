197e4592b85d0ee5e8bf731050a85c8342ec18ca
timama <timforworking@mail.ru>
2024-02-07 10:35:32 +0300

    push

--- job_openings_app/filters/job_opening_filters.py
+++ job_openings_app/filters/job_opening_filters.py
@@ -1,16 +1,18 @@
+import django_filters
 from django_filters import rest_framework as filters
 from job_openings_app.models import JobOpening
 
 
 class JobOpeningFilter(filters.FilterSet):
+    job_activity = django_filters.CharFilter(field_name='job_activity__id', lookup_expr='in')
+    job_category = django_filters.CharFilter(field_name='job_category__id', lookup_expr='in')
+
     class Meta:
         model = JobOpening
         fields = {
             'employer': ['exact'],
             'applicants': ['exact'],
             'job_type': ['exact'],
-            'job_category': ['exact'],
-            'job_activity': ['exact'],
             'archived': ['exact'],
             'employee_found': ['exact'],
         }
