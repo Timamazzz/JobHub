285991d37ad65fac406c0a1429c01db46ba9f12f
timama <timforworking@mail.ru>
2024-02-07 13:44:51 +0300

    push

--- job_openings_app/filters/job_opening_filters.py
+++ job_openings_app/filters/job_opening_filters.py
@@ -1,27 +1,8 @@
 from django_filters import rest_framework as filters
+
+from JobHub.utils.filters import MultipleValueFilter
 from job_openings_app.models import JobOpening
-from django_filters.filters import Filter
-from django.forms.fields import MultipleChoiceField, IntegerField
-
-
-class MultipleValueField(MultipleChoiceField):
-    def __init__(self, *args, field_class, **kwargs):
-        self.inner_field = field_class()
-        super().__init__(*args, **kwargs)
-
-    def valid_value(self, value):
-        return self.inner_field.validate(value)
-
-    def clean(self, values):
-        return values and [self.inner_field.clean(value) for value in values]
-
-
-class MultipleValueFilter(Filter):
-    field_class = MultipleValueField
-
-    def __init__(self, *args, field_class, **kwargs):
-        kwargs.setdefault('lookup_expr', 'in')
-        super().__init__(*args, field_class=field_class, **kwargs)
+from django.forms.fields import IntegerField
 
 
 class JobOpeningFilter(filters.FilterSet):
