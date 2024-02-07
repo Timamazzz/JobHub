d2be6918bb40e94746774fa7d9f859d89d776e21
timama <timforworking@mail.ru>
2024-02-07 17:34:22 +0300

    push

--- employers_app/urls.py
+++ employers_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView
-from employers_app.views import EmployerViewSet
+from employers_app.views import EmployerViewSet, SendToModerationAPIView
 
 router = DefaultRouter()
 router.register(r'', EmployerViewSet)
 
 urlpatterns = [
+    path('send-to-moderation/', SendToModerationAPIView.as_view()),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', include(router.urls)),
 ]
--- job_openings_app/urls.py
+++ job_openings_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from employers_app.views import SendToModerationAPIView
 from job_openings_app.views import JobOpeningViewSet, JobCategoryViewSet, JobActivityViewSet
 
 router = DefaultRouter()
@@ -11,6 +10,5 @@ router.register(r'', JobOpeningViewSet)
 
 
 urlpatterns = [
-    path('a/send-to-moderation/', SendToModerationAPIView.as_view()),
     path('', include(router.urls)),
 ]
