8be8dd470b37490b6e7b3f69017452814970ae60
timama <timforworking@mail.ru>
2024-02-09 15:59:51 +0300

    push

--- job_openings_app/pagination.py
+++ job_openings_app/pagination.py
@@ -5,9 +5,11 @@ from rest_framework.response import Response
 class CustomPagination(pagination.PageNumberPagination):
     def get_paginated_response(self, data):
         return Response({
+            'count': self.page.paginator.count,
+            'pages': self.page.paginator.num_pages,
+
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
-            'pages': self.page.paginator.num_pages,
-            'count': self.page.paginator.count,
+
             'results': data
         })
