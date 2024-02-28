cc25f4f14fffe75e167da70395eedf43152403b6
timama <timforworking@mail.ru>
2024-02-28 12:18:33 +0300

    push

--- JobHub/celery.py
+++ JobHub/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
-app = Celery('proj')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JobHub.settings')
+app = Celery('JobHub')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
