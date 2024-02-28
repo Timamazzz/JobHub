3ac4a790af67d0a1d6bcd27f188de1e87e727260
timama <timforworking@mail.ru>
2024-02-28 12:56:49 +0300

    push

--- job_openings_app/tasks.py
+++ job_openings_app/tasks.py
@@ -6,7 +6,6 @@ from .models import JobOpening
 
 @shared_task(name='archive_old_job_openings')
 def archive_old_job_openings():
-    # threshold_date = timezone.now() - timedelta(days=30)
-    # JobOpening.objects.filter(created_at__lte=threshold_date, archived=False).update(archived=True)
-    print('Iam tasks archived')
+    threshold_date = timezone.now() - timedelta(days=30)
+    JobOpening.objects.filter(created_at__lte=threshold_date, archived=False).update(archived=True)
 
