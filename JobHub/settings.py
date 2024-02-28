51bc1055fd9b7f7d183ddbfe364b90a7e736fb0f
timama <timforworking@mail.ru>
2024-02-28 12:15:48 +0300

    push

--- JobHub/settings.py
+++ JobHub/settings.py
@@ -215,10 +215,3 @@ AFTER_VK_AUTH_REDIRECT_REGISTER = '/profile/'
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-
-CELERY_BEAT_SCHEDULE = {
-    'run-daily-task': {
-        'task': 'job_openings_app.tasks.archive_old_job_openings',
-        'schedule': crontab(minute='0', hour='0'),
-    },
-}
