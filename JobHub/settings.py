53fdeea19a4c0a0a317e31912c91ea0bc64e25e9
timama <timforworking@mail.ru>
2024-02-28 12:11:10 +0300

    push

--- JobHub/settings.py
+++ JobHub/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+
+from celery.schedules import crontab
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -212,3 +214,10 @@ AFTER_VK_AUTH_REDIRECT_REGISTER = '/profile/'
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+CELERY_BEAT_SCHEDULE = {
+    'run-daily-task': {
+        'task': 'job_openings_app.tasks.archive_old_job_openings',
+        'schedule': crontab(minute='0', hour='0'),
+    },
+}
---
+++ job_openings_app/tasks.py
@@ -0,0 +1,11 @@
+from datetime import timedelta
+from django.utils import timezone
+from celery import shared_task
+from .models import JobOpening
+
+
+@shared_task
+def archive_old_job_openings():
+    threshold_date = timezone.now() - timedelta(days=30)
+    JobOpening.objects.filter(created_at__lte=threshold_date, archived=False).update(archived=True)
+
