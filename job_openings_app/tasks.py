dde770fdf46e554cc4723f9f3d280fc07c0c3162
timama <timforworking@mail.ru>
2024-02-28 12:16:35 +0300

    push

--- job_openings_app/tasks.py
+++ job_openings_app/tasks.py
@@ -4,7 +4,7 @@ from celery import shared_task
 from .models import JobOpening
 
 
-@shared_task
+@shared_task(name='archive_old_job_openings')
 def archive_old_job_openings():
     threshold_date = timezone.now() - timedelta(days=30)
     JobOpening.objects.filter(created_at__lte=threshold_date, archived=False).update(archived=True)
