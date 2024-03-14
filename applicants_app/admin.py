e0561f08f339fbb5306af65f3215f40f4557b9ce
Timama <timforworking@mail.ru>
2024-03-14 20:40:12 +0300

    fix

--- applicants_app/admin.py
+++ applicants_app/admin.py
@@ -1,11 +1,38 @@
 from django.contrib import admin
 from .models import Applicant
-
+from django.http import HttpResponse
+import openpyxl
 
 class ApplicantAdmin(admin.ModelAdmin):
     list_display = ('user', 'fio', 'birth_date', 'phone_number', 'email', 'vk_id')
     search_fields = ('user__username', 'fio', 'phone_number', 'email', 'vk_id')
     list_filter = ('birth_date',)
 
+    def export_to_excel(self, request, queryset):
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        ws.title = "Соискатели"
+
+        headers = ['ID', 'ФИО', 'Дата рождения', 'Номер телефона', 'Email', 'Резюме', 'ID ВКонтакте']
+        ws.append(headers)
+
+        for applicant in queryset:
+            row = [
+                applicant.id,
+                applicant.fio,
+                applicant.birth_date,
+                applicant.phone_number,
+                applicant.email,
+                applicant.resume,
+                applicant.vk_id
+            ]
+            ws.append(row)
+
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=applicants.xlsx'
+        wb.save(response)
+        return response
+
+    export_to_excel.short_description = "Экспорт в Excel"
 
 admin.site.register(Applicant, ApplicantAdmin)
--- employers_app/admin.py
+++ employers_app/admin.py
@@ -1,9 +1,62 @@
+from job_openings_app.models import JobOpening
 from django.contrib import admin
+from django.http import HttpResponse
+import openpyxl
 from .models import Employer
-
 
 @admin.register(Employer)
 class EmployerAdmin(admin.ModelAdmin):
     list_display = ('name', 'inn', 'legal_address', 'phone_number', 'email')
     search_fields = ('name', 'inn', 'phone_number', 'email')
     list_filter = ('name',)
+
+    def export_to_excel(self, request, queryset):
+        wb = openpyxl.Workbook()
+        ws = wb.active
+
+        ws.append([
+            'Название компании',
+            'ИНН',
+            'Юридический адрес',
+            'Номер телефона',
+            'Email',
+            'Описание',
+            'Сайт',
+            'Название вакансии',
+            'Описание вакансии',
+            'Кол-во откликнувшихся соискателей'
+        ])
+
+        for employer in queryset:
+            ws.append([
+                employer.name,
+                employer.inn,
+                employer.legal_address,
+                employer.phone_number,
+                employer.email,
+                employer.description,
+                employer.site if employer.site else '',
+            ])
+
+            job_openings = JobOpening.objects.filter(employer=employer)
+            for job_opening in job_openings:
+                ws.append([
+                    '',
+                    '',
+                    '',
+                    '',
+                    '',
+                    '',
+                    '',
+                    job_opening.title,
+                    job_opening.description,
+                    job_opening.applicants.count()
+                ])
+
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=employers_data.xlsx'
+        wb.save(response)
+        return response
+
+    export_to_excel.short_description = "Экспорт в Excel"
+
